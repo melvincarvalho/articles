3de2838cb47e3b07c2db9e6719f1a834ba2bd24e
Melvin Carvalho <melvincarvalho@gmail.com>
2019-11-25 09:14:06 +0100

    start countdown timer react hooks

---
+++ 2019/11/countdown-timer-react-hooks.mm
@@ -0,0 +1,101 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="countdown-timer-react-hooks" FOLDED="false" ID="ID_1831615648" CREATED="1569272472516" MODIFIED="1574669245908" ICON_SIZE="20.0 pt" LINK="index.mm" STYLE="oval">
+<icon BUILTIN="bookmark"/>
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Summary" POSITION="right" ID="ID_1226961462" CREATED="1570979963389" MODIFIED="1570980209554" COLOR="#3333ff">
+<icon BUILTIN="wizard"/>
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#0000ff"/>
+<node TEXT="Also, with some major holidays as well as the New Year approaching, &#x2018;tis the season for building a countdown timer!" ID="ID_1869070715" CREATED="1574669488909" MODIFIED="1574669488909"/>
+</node>
+<node TEXT="Details" POSITION="right" ID="ID_120250381" CREATED="1570979967089" MODIFIED="1570980228711">
+<icon BUILTIN="list"/>
+<edge COLOR="#00ff00"/>
+<font BOLD="true"/>
+<node TEXT="Getting Started" ID="ID_1518855550" CREATED="1574669507787" MODIFIED="1574669511470"/>
+<node TEXT="Calculating How Much Time is Left" ID="ID_962556769" CREATED="1574669518222" MODIFIED="1574669547461"/>
+<node TEXT="Hooking Up the State and Effect" ID="ID_1580346473" CREATED="1574669534349" MODIFIED="1574669542630"/>
+<node TEXT="Displaying the Time Left" ID="ID_1223853790" CREATED="1574669553431" MODIFIED="1574669557348"/>
+<node TEXT="Putting it All Together" ID="ID_1848906989" CREATED="1574669562947" MODIFIED="1574669568081"/>
+</node>
+<node TEXT="links" POSITION="left" ID="ID_808607355" CREATED="1570968728188" MODIFIED="1570968728190">
+<icon BUILTIN="attach"/>
+<edge COLOR="#00007c"/>
+<node TEXT="countdown-timer-react-hooks" ID="ID_147800645" CREATED="1574669160421" MODIFIED="1574669395721" LINK="https://alligator.io/react/countdown-timer-react-hooks/"/>
+<node TEXT="demo" ID="ID_1605438661" CREATED="1574669594439" MODIFIED="1574669598330" LINK="https://codesandbox.io/s/funny-sinoussi-howyc"/>
+</node>
+<node TEXT="docs" POSITION="left" ID="ID_1100215200" CREATED="1570969039178" MODIFIED="1570980265221">
+<icon BUILTIN="folder"/>
+<edge COLOR="#ff00ff"/>
+<node TEXT="introduction-to-react-hooks" ID="ID_1510512159" CREATED="1574669378226" MODIFIED="1574669417851" LINK="https://alligator.io/react/react-hooks/"/>
+</node>
+<node TEXT="tags" POSITION="left" ID="ID_466759422" CREATED="1570968754383" MODIFIED="1571340239144">
+<icon BUILTIN="hashtag"/>
+<edge COLOR="#007c00"/>
+<node TEXT="#react" ID="ID_726822941" CREATED="1574669215834" MODIFIED="1574669360331"/>
+</node>
+</node>
+</map>
